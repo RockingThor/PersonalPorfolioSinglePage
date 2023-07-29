6054ebf0999fb4c1ef05dc9aedddc88a8317442d
Rohit Nandi <rohitnandibmp@gmail.com>
2023-07-29 14:48:08 +0530

    done with the navbar and shared elements

--- src/components/intro/index.jsx
+++ src/components/intro/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './style.scss';
+import cloud from '../../images/cloud.png';
+import cloudSoft from '../../images/cloud-soft.png';
+import Navigation from './navigation-bar';
 
 const Intro = () => {
   return (
-    <div>index</div>
+    <div className="Intro-section">
+      <div className="vector-bg" id="parallax">
+
+      </div>
+        <img src={cloud} alt="cloud" className='cloud' />
+        <img src={cloudSoft} alt="cloud-soft" className="cloud-soft" />
+        <div className="content">
+
+        </div>
+        <Navigation/>
+    </div>
   )
 }
 
